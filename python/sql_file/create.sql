377fa5a55ca8149de4119e95fb9ec19e4a1847e3
Ewan <ewan@MacBook-Air-de-Ewan.local>
2026-01-29 16:39:01 +0100

    ajout data

--- python/sql_file/create.sql
+++ python/sql_file/create.sql
@@ -1,4 +1,12 @@
-INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Silver Star', 'Montagne russe', 3, 1);
-INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Montagne 8', 'Montagne russe', 4, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Silver Star', 'Montagne russe hypercoaster à Europa-Park (Allemagne)', 5, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Blue Fire', 'Montagne russe à lancement à Europa-Park (Allemagne)', 4, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Wodan Timburcoaster', 'Montagne russe en bois à Europa-Park (Allemagne)', 4, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Voltron Nevera', 'Montagne russe à propulsion multiple à Europa-Park (Allemagne)', 5, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Shambhala', 'Montagne russe emblématique à PortAventura Park (Espagne)', 5, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Dragon Khan', 'Montagne russe avec loopings à PortAventura Park (Espagne)', 5, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Furius Baco', 'Montagne russe accélération rapide à PortAventura Park (Espagne)', 5, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Toutatis', 'Montagne russe à Parc Astérix (France)', 4, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Untamed', 'Montagne russe à Walibi Holland (Pays-Bas)', 4, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Batman Gotham City Escape', 'Montagne russe à Parque Warner Madrid (Espagne)', 4, 1);
 
 INSERT INTO users (name, password) VALUES ('toto', 'toto');
